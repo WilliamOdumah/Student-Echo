e80c57994db93651d20a0953a0522ddb09586200
Rishamdeep Singh <singhr50@mumanitoba.ca>
2024-02-05 19:41:21 -0600

    Login page

---
+++ app/src/main/java/comp3350/student_echo/presentation/MainActivity_Login.java
@@ -0,0 +1,16 @@
+package comp3350.student_echo.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp3350.student_echo.R;
+
+public class MainActivity_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+    }
+}
