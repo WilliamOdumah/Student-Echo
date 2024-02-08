7fb7e61ce35cfce66c51ba4485cfc0cc8fa0ab32
kelly <villamak@myumanitoba.ca>
2024-02-08 14:04:13 -0600

    changed Activity_Login.java into LoginActivity.java completed the object unit tests <3

--- app/src/test/java/comp3350/student_echo/tests/objects/CourseReviewTest.java
+++ app/src/test/java/comp3350/student_echo/tests/objects/CourseReviewTest.java
@@ -1,14 +1,49 @@
 package comp3350.student_echo.tests.objects;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import comp3350.student_echo.objects.Course;
+import comp3350.student_echo.objects.CourseReview;
+import comp3350.student_echo.objects.Review;
+import comp3350.student_echo.objects.StudentAccount;
+
 public class CourseReviewTest {
-    @Test
-    public void testCourseReview() {
-        //TODO
-        assertEquals(1, 1);
+    private Course course;
+    private StudentAccount author;
+    private Review courseReview;
+
+    @Before
+    public void before() {
+        course = new Course("Science", "COMP3010", "Distributed Systems");
+        author = new StudentAccount("kelly", "villamayor", "villamak@myumanitoba.ca");
+        courseReview = new CourseReview(course,"mid", 3, 4, author);
     }
 
+    @Test
+    public void testGetDifficultyRating() {
+        assertEquals("checking course review's difficulty rating", courseReview.getDifficultyRating(), 4);
+        assertNotEquals("checking course review's difficulty rating", courseReview.getDifficultyRating(), 1);
+    }
+
+    @Test
+    public void testGetWrittenBy() {
+        assertEquals("checking course review's author", courseReview.getWrittenBy(), "kelly");
+        assertNotEquals("checking course review's author", courseReview.getWrittenBy(), "a");
+    }
+
+    @Test
+    public void testGetComment() {
+        assertEquals("checking course review's comment", courseReview.getComment(), "mid");
+        assertNotEquals("checking course review's comment", courseReview.getComment(), "notmid");
+    }
+
+    @Test
+    public void testGetOverallRating() {
+        assertEquals("checking course review's overall rating", courseReview.getOverallRating(), 3);
+        assertNotEquals("checking course review's overall rating", courseReview.getOverallRating(), 1);
+    }
 }
--- app/src/test/java/comp3350/student_echo/tests/objects/InstructorReviewTest.java
+++ app/src/test/java/comp3350/student_echo/tests/objects/InstructorReviewTest.java
@@ -1,13 +1,49 @@
 package comp3350.student_echo.tests.objects;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import comp3350.student_echo.objects.Instructor;
+import comp3350.student_echo.objects.InstructorReview;
+import comp3350.student_echo.objects.Review;
+import comp3350.student_echo.objects.StudentAccount;
 
 public class InstructorReviewTest {
-    @Test
-    public void testInstructorReview() {
-        //TODO
-        assertEquals(1, 1);
+    private Instructor instructor;
+    private StudentAccount author;
+    private Review instructorReview;
+
+    @Before
+    public void before() {
+        instructor = new Instructor("Internet Friend", "Rob", "Guderian");
+        author = new StudentAccount("kelly", "villamayor", "villamak@myumanitoba.ca");
+        instructorReview = new InstructorReview(instructor, "a very good farmer", 4, 3, author);
     }
 
+    @Test
+    public void testGetDifficultyRating() {
+        assertEquals("checking instructor review's difficulty rating", instructorReview.getDifficultyRating(), 3);
+        assertNotEquals("checking instructor review's difficulty rating", instructorReview.getDifficultyRating(), 1);
+    }
+
+    @Test
+    public void testGetWrittenBy() {
+        assertEquals("checking instructor review's author", instructorReview.getWrittenBy(), "kelly");
+        assertNotEquals("checking instructor review's author", instructorReview.getWrittenBy(), "a");
+    }
+
+    @Test
+    public void testGetComment() {
+        assertEquals("checking instructor review's comment", instructorReview.getComment(), "a very good farmer");
+        assertNotEquals("checking instructor review's comment", instructorReview.getComment(), "empty");
+    }
+
+    @Test
+    public void testGetOverallRating() {
+        assertEquals("checking instructor review's overall rating", instructorReview.getOverallRating(), 4);
+        assertNotEquals("checking instructor review's overall rating", instructorReview.getOverallRating(), 1);
+    }
 }
