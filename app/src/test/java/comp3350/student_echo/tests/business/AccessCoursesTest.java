9c3d983b2e6993bfbcb470cf9dce20ba76bce84d
kelly <villamak@myumanitoba.ca>
2024-02-07 22:16:13 -0600

    Added AccessAccountsTest.java and everything passed (woo!!)
    Removed dead-code in AccountPersistenceStub.java (insert, delete, update account functions)
    Removed dead-code in AccessCourses.java (insert, delete, update course functions)
    ^ these are meant for iteration 2
    
    Removed dead-code in AccessCourses.java (getSequential function)

--- app/src/test/java/comp3350/student_echo/tests/business/AccessCoursesTest.java
+++ app/src/test/java/comp3350/student_echo/tests/business/AccessCoursesTest.java
@@ -1,8 +1,6 @@
 package comp3350.student_echo.tests.business;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -26,51 +24,9 @@ public class AccessCoursesTest {
     }
 
     @Test
-    public void testListCourses() {
-
-        final Course course;
-        course = accessCourses.getSequential();
-
-        assertNotNull("first sequential course should not be null", course);
-
-        assertTrue("COMP1020".equals(course.getCourseID()));
-
-        System.out.println("Finished test AccessCourses\n");
-    }
-
-    @Test
     public void testGetCourses() {
         final List<Course> courses = accessCourses.getCourses();
         assertEquals(10, courses.size());
         System.out.println("Finished test Get Courses\n");
-    }
-
-    @Test
-    public void testDeleteCourse() {
-        final Course c = accessCourses.getSequential();
-        List<Course> courses = accessCourses.getCourses();
-        assertEquals(11, courses.size());
-        accessCourses.deleteCourse(c);
-        courses = accessCourses.getCourses();
-        assertEquals(10, courses.size());
-        System.out.println("Finished test Delete Course\n");
-    }
-
-    @Test
-    public void testInsertCourse() {
-        final Course c = new Course("Business", "ACC 1100", "Introductory Financial Accounting");
-        accessCourses.insertCourse(c);
-        assertEquals(11, accessCourses.getCourses().size());
-
-        System.out.println("Finished test Insert Course\n");
-    }
-
-    @Test
-    public void testUpdateCourse() {
-        final Course c = accessCourses.getSequential();
-        final Course u = new Course(c.getDepartment(),c.getCourseID(), "A new name");
-        accessCourses.updateCourse(u);
-        assertEquals(11, accessCourses.getCourses().size());
-        System.out.println("Finished test Update Course\n");
     }
 }
--- app/src/test/java/comp3350/student_echo/tests/stubs/AccountPersistenceStubTest.java
+++
@@ -1,5 +0,0 @@
-package comp3350.student_echo.tests.stubs;
-
-public class AccountPersistenceStubTest {
-
-}
