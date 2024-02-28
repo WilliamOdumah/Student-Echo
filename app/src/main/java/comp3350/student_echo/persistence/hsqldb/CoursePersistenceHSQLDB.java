5853762b8b43b598c3009ef9aa154981f2b9656f
brettaseltine <59149406+brettaseltine@users.noreply.github.com>
2024-02-28 16:18:17 -0600

    working CoursePersistenceHSQLDB

--- app/src/main/java/comp3350/student_echo/persistence/hsqldb/CoursePersistenceHSQLDB.java
+++ app/src/main/java/comp3350/student_echo/persistence/hsqldb/CoursePersistenceHSQLDB.java
@@ -1,13 +1,56 @@
 package comp3350.student_echo.persistence.hsqldb;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+import java.util.ArrayList;
+import android.util.Log;
 
 import comp3350.student_echo.objects.Course;
 import comp3350.student_echo.persistence.CoursePersistence;
 
 public class CoursePersistenceHSQLDB implements CoursePersistence {
+    private final String dbPath;
+
+    public CoursePersistenceHSQLDB(final String dbPath) {
+        this.dbPath = dbPath;
+    }
+
+    private Connection connection() throws SQLException {
+        return DriverManager.getConnection("jdbc:hsqldb:file:" + dbPath + ";shutdown=true", "SA", "");
+    }
+
+    private Course fromResultSet(final ResultSet rs) throws SQLException {
+        final String courseID = rs.getString("courseID");
+        final String courseName = rs.getString("courseName");
+        final String department = rs.getString("department");
+        return new Course(department, courseID, courseName);
+    }
+
     @Override
-    public List<Course> getCourseSequential() {
+    public List<Course> getCourseSequential(){
+        final List<Course> courses = new ArrayList<>();
+
+        try (final Connection c = connection()) {
+            final Statement st = c.createStatement();
+            final ResultSet rs = st.executeQuery("SELECT * FROM courses");
+            while (rs.next()) {
+                final Course course = fromResultSet(rs);
+                courses.add(course);
+            }
+            rs.close();
+            st.close();
+            return courses;
+        }
+        catch (final SQLException e) {
+            Log.e("Connect SQL", e.getMessage() + e.getSQLState());
+            e.printStackTrace();
+        }
         return null;
     }
+
 }
--- app/src/main/java/comp3350/student_echo/presentation/LoginActivity.java
+++ app/src/main/java/comp3350/student_echo/presentation/LoginActivity.java
@@ -1,6 +1,8 @@
 package comp3350.student_echo.presentation;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -8,9 +10,14 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import comp3350.student_echo.R;
+import comp3350.student_echo.application.Main;
 import comp3350.student_echo.business.LoginManager;
-import comp3350.student_echo.objects.StudentAccount;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        copyDatabaseToDevice();
     }
     @Override
     protected void onDestroy() {
@@ -48,5 +56,59 @@ public class LoginActivity extends AppCompatActivity {
     public void buttonSignupOnClick(View v){
         Intent signUpIntent= new Intent(LoginActivity.this, SignUpActivity.class);
         LoginActivity.this.startActivity(signUpIntent);
+    }
+
+    // DB STUFF FROM SAMPLE PROJECT BELOW
+    private void copyDatabaseToDevice() {
+        final String DB_PATH = "db";
+
+        String[] assetNames;
+        Context context = getApplicationContext();
+        File dataDirectory = context.getDir(DB_PATH, Context.MODE_PRIVATE);
+        AssetManager assetManager = getAssets();
+
+        try {
+
+            assetNames = assetManager.list(DB_PATH);
+            for (int i = 0; i < assetNames.length; i++) {
+                assetNames[i] = DB_PATH + "/" + assetNames[i];
+            }
+
+            copyAssetsToDirectory(assetNames, dataDirectory);
+
+            Main.setDBPathName(dataDirectory.toString() + "/" + Main.getDBPathName());
+
+        } catch (final IOException ioe) {
+            //Messages.warning(this, "Unable to access application data: " + ioe.getMessage());
+            //TODO: UPDATE ERROR MESSAGE
+            System.out.println("Unable to access application data: " + ioe.getMessage());
+        }
+    }
+    public void copyAssetsToDirectory(String[] assets, File directory) throws IOException {
+        AssetManager assetManager = getAssets();
+
+        for (String asset : assets) {
+            String[] components = asset.split("/");
+            String copyPath = directory.toString() + "/" + components[components.length - 1];
+
+            char[] buffer = new char[1024];
+            int count;
+
+            File outFile = new File(copyPath);
+
+            if (!outFile.exists()) {
+                InputStreamReader in = new InputStreamReader(assetManager.open(asset));
+                FileWriter out = new FileWriter(outFile);
+
+                count = in.read(buffer);
+                while (count != -1) {
+                    out.write(buffer, 0, count);
+                    count = in.read(buffer);
+                }
+
+                out.close();
+                in.close();
+            }
+        }
     }
 }
