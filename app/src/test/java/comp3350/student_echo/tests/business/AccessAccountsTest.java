6c238f325faa724728cdcfe11b8c6c967c933eea
brettaseltine <59149406+brettaseltine@users.noreply.github.com>
2024-03-07 15:17:09 -0600

    testing

--- app/src/test/java/comp3350/student_echo/tests/business/AccessAccountsTest.java
+++ app/src/test/java/comp3350/student_echo/tests/business/AccessAccountsTest.java
@@ -1,8 +1,8 @@
 package comp3350.student_echo.tests.business;
 
-
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -61,5 +61,37 @@ public class AccessAccountsTest {
         assertNotNull("testing with newly-added account", accessAccounts.getAccount("a"));
 
         assertNull("testing with non-existent account", accessAccounts.getAccount("b"));
+    }
+
+    @Test
+    public void getAccountUsingEmailTest() {
+        StudentAccount expected = new StudentAccount("brett","aseltine","brett@myumanitoba.ca");
+        StudentAccount retrieved = accessAccounts.getAccountUsingEmail("brett@myumanitoba.ca");
+        assertTrue("Retrieve expected account", expected.equals(retrieved));
+
+        StudentAccount notExisting = new StudentAccount("no","no","no@myumanitoba.ca");
+        assertNull("dont get not existing account", accessAccounts.getAccountUsingEmail("no@myumanitoba.ca"));
+    }
+
+    @Test
+    public void updateAccountTest() {
+        StudentAccount acc = new StudentAccount("brett","aseltine","brett@myumanitoba.ca");
+        acc.setUsername("yo");
+        accessAccounts.updateAccount(acc);
+        assertTrue("Verify username update occurs", acc.getUsername().equals("yo"));
+
+        acc.setPassword("newPass");
+        accessAccounts.updateAccount(acc);
+        assertTrue("Verify password update occurs", acc.getPassword().equals("newPass"));
+
+    }
+
+    // delete account
+    @Test
+    public void deleteAccountTest() {
+        StudentAccount toDelete = new StudentAccount("brett","aseltine","brett@myumanitoba.ca");
+        accessAccounts.deleteAccount(toDelete);
+        assertNull("ensure account removed", accessAccounts.getAccount("brett"));
+        assertNull("deleting non-existing account", accessAccounts.getAccount("not existing"));
     }
 }
