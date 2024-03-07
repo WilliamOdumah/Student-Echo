6a6b1309f3858605e3c22d48cf763d105ebbbc2c
Rishamdeep Singh <singhr50@mumanitoba.ca>
2024-03-06 23:36:35 -0600

    Integration test for AccessAccounts

--- app/src/test/java/comp3350/student_echo/tests/business/AccessAccountsTest.java
+++ app/src/test/java/comp3350/student_echo/tests/business/AccessAccountsTest.java
@@ -4,36 +4,39 @@ package comp3350.student_echo.tests.business;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
 import comp3350.student_echo.business.access.AccessAccounts;
 import comp3350.student_echo.objects.StudentAccount;
+import comp3350.student_echo.persistence.AccountPersistence;
+import comp3350.student_echo.persistence.hsqldb.AccountPersistenceHSQLDB;
+import comp3350.student_echo.persistence.stubs.AccountPersistenceStub;
 
 public class AccessAccountsTest {
 
-    private final AccessAccounts accessAccounts;
+    private AccessAccounts accessAccounts;
+    @Before
+    public void setUp() {
+        AccountPersistence stub = new AccountPersistenceStub();
 
-    public AccessAccountsTest() {
+        accessAccounts = new AccessAccounts(stub);
 
-        accessAccounts = new AccessAccounts();
-
-        getAccountsTest(); // testing on unmodified stub
-        getAccountTest();
-        addAccountsTest();
-        getAccountsTest(); // testing on modified stub
     }
 
     @Test
-    // testing if getAccounts works for unmodified stub
     public void getAccountsTest() {
+
         List<StudentAccount> accounts = accessAccounts.getAccounts();
 
-        for(StudentAccount cur: accounts) {
-            String username = cur.getUsername();
+        for(int i=0;i<accounts.size();i++) {
+            String username = accounts.get(i).getUsername();
             assertNotNull("testing if " +  username + "'s account exists", accessAccounts.getAccount(username));
         }
+
+
     }
 
     @Test
